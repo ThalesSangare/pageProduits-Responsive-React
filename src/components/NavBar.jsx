2ae35c2a68de226b70fb07a23ea61c1c161492d9
Thales7 <sangarekabinet7@gmail.com>
2026-02-27 01:09:24 +0100

    debut responsive NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,57 @@
+import { HandCoins, Search, ShoppingCart, UserRound } from "lucide-react";
+
 const liens = [
   { nom: "Accueil", url: "/" },
   { nom: "Produits", url: "/produits" },
+  { nom: "A propos", url: "/produits" },
   { nom: "Contact", url: "/contact" },
+  { nom: "Connexion", url: "/contact" },
 ];
 
 function NavBar() {
   return (
-    <div className="flex justify-center items-center w-full h-32 p-4 bg-cyan-950 mb-8">
-      <ul className="flex gap-4 justify-center items-center">
-        {liens.map((lien) => (
-          <li key={lien.nom} className="hover:text-accent transition duration-300 cursor-pointer">
-            {lien.nom}
+    // <div className="hidden md:flex px-6 py-4 bg-cyan-950 mb-8">
+    <div className="max-w-7xl mx-auto flex items-center px-6 py-4 mb-8">
+      {/* LOGO */}
+      <div className="flex-1 flex">
+        <HandCoins />
+        <h1 className="ml-2 text-xl text-accent font-bold">E-commerce</h1>
+      </div>
+
+      {/* LIENS */}
+      <div className="flex-1 hidden md:flex justify-center">
+        <ul className="flex gap-4">
+          {liens.map((lien) => (
+            <li
+              key={lien.nom}
+              className="hover:text-accent transition duration-300 cursor-pointer"
+            >
+              {lien.nom}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/*ICONES  */}
+      <div className="flex-1 flex justify-end ">
+        <ul className="flex flex gap-4">
+          <li>
+            <a href="#" className="hover:text-accent transition duration-300">
+              <Search />
+            </a>
           </li>
-        ))}
-      </ul>
+          <li>
+            <a href="#" className="hover:text-accent transition duration-300">
+              <ShoppingCart />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-accent transition duration-300">
+              <UserRound />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
