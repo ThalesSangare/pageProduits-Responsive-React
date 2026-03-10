a349f972890d073a8356ea15c85c1bad474c564a
Thales7 <sangarekabinet7@gmail.com>
2026-03-11 00:05:31 +0100

    Restructuration du projet (Debut de la separation du front au Back)

--- backEnd/ecommerce_backend/src/main/java/com/komah/ecommerce_backend/repository/CategorieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.komah.ecommerce_backend.repository;
-
-import com.komah.ecommerce_backend.entities.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-}
